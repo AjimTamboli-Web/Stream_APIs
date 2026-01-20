139ef2c5d4f4a21bc5bba1ddba49ad0d9bd24c0d
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-01-20 17:55:10 +0530

    limit and skip stream method

--- operations_Over_Stream/B_Intermediate_Operations.java
+++ operations_Over_Stream/B_Intermediate_Operations.java
@@ -18,6 +18,10 @@ public class B_Intermediate_Operations {
  *      ⭕ map() - Transform each element,   ⭕ filter() - Select elements based on condition, 
  *      ⭕ sorted() - Sort elements,         ⭕ flatMap() - Flatten nested structures, 
  *      ⭕ distinct() - Remove duplicates,   ⭕ peek() - Debug / perform action without changing stream
+ *      ⭕ limit(n) - Take first n elements  ⭕ skip(n) - Ignore first n elements
+ *           >> limit() & skip() This combo is commonly used for pagination. => skip = (pageNumber - 1) * pageSize
+ *           >> ✔ Order of operations matters in limit and skip () 
+ *     
  *      
  * These operations do not produce a result immediately.
  * They return a new Stream and are lazy (executed only when a terminal operation is called).
@@ -48,13 +52,24 @@ public class B_Intermediate_Operations {
 						.sorted((i,j) -> {return Integer.compare(j, i); } )  // Comparator implementing
 //				sorted(Comparator?)	: Sorts the elements using comparator Interface like custom sorting.	
 						
-						.peek((c) -> {System.out.println("$" + c);})  // Consumer implementing
+						.peek((c) -> {System.out.println(">>" + c);})  // Consumer implementing
 // 				Debug /perform action without changing stream,
 //				Performs a specified action on each element of the Stream without consuming the elements.	
 						
 						.distinct()    // for custom class is maintaining using hashCode and .equals override
 //				Removes duplicate elements from the Stream.	 It returns a stream consisting of the 
 //						distinct elements (according to Object.equals(Object)).	
+						
+						.skip(2)  // (Skips) Ignore the first n elements, Process the remaining elements
+//	 If this stream contains fewer than n elements then an empty stream will be returned.
+//						✔ skip() consumes elements silently
+						
+						
+						.limit(5)  // limit() is mandatory for infinite streams.⚠ Without limit() → infinite loop
+//	It returns a stream consisting of the elements of this stream, truncated to be no longer than maxSize in length.
+//  						✔ limit() short-circuits the stream
+						
+						
 						
 						.forEach((f) -> System.out.println(f)); // Terminal operations  Consumer implementing
 		
