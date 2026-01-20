39e5f887e5debd60a9c86583d426e3c0845f2da8
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-01-20 17:56:52 +0530

    Count() method

--- operations_Over_Stream/C_Terminal_Operations.java
+++ operations_Over_Stream/C_Terminal_Operations.java
@@ -6,14 +6,21 @@ public class C_Terminal_Operations {
 
 	public static void main(String[] args) {
 		
+//		 ⭕ count() - Returns the count of elements in the stream.
+		// It returns the count of elements in this stream. This is a special case of a reduction.
+		
 		System.out.println(
 		List.of(3,6,58,9,2,9,54,1,75)
 		.stream()
 		
-		.anyMatch((a) -> {return a < 10;})
+//		.anyMatch((a) -> {return a < 10;})
+		
 		
 //		.allMatch((i) -> {return i > 7;})
 		
+		
+		
+		.count() // return long value Returns the count of elements in the stream.
 				);
 	}
 
